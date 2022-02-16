b586010f0264f7927e3e02e1eecc78d1d422d27e
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-02-16 08:38:52 -0700

    finished

---
+++ popGrowth.js
@@ -0,0 +1,16 @@
+function nbYear(population, percent, newInhabit, final) {
+    let pop = population
+    let growth = (percent / 100)
+    let years = 0
+
+    while (pop < final) {
+        pop = pop + pop * growth + newInhabit
+        years += 1
+    }
+    
+    console.log(years)
+}
+
+nbYear(1000, 2, 50, 1200)
+nbYear(1500, 5, 100, 5000)
+nbYear(1500000, 2.5, 10000, 2000000)
